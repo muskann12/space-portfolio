c1651c87571b0a5261a03300f9bad4f476d10591
muskann12 <nisarmuskan14@gmail.com>
2024-11-13 19:26:29 +0500

    Added video controls and fixed autoplay issues

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import HeroContent from '../sub/HeroContent';
+import React from 'react'
+import HeroContent from '../sub/HeroContent'
 
 const Hero = () => {
   return (
-    <div className='relative flex flex-col h-screen w-full'>
-      {/* Video Background */}
-      <video
+    <div className='relative flex flex-col h-full w-full'>
+
+        <video 
         autoPlay
         muted
         loop
-        playsInline // Ensures that the video plays inline on mobile without showing controls
-        className='absolute top-0 left-0 z-0 w-full h-full object-cover'
-      >
-        <source src='/images/blackhole.webm' type='video/webm' />
-        <source src='/images/blackhole.mp4' type='video/mp4' />
-        <p>Your browser does not support the video format.</p>
-      </video>
-
-      {/* Hero Content */}
-      <HeroContent />
+        className='rotate-100 absolute top-[-340px] left-0 z-[1] w-full h-full object-cover'
+        controls={false}  // Remove controls from video
+        >
+            <source src='/images/blackhole.webm' type='video/webm'/>
+        </video>
+        <HeroContent />
     </div>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
